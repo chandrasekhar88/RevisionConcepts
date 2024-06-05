1f00193c5e6002a7995dfbc19d7b351857e615d9
chandrasekhar88 <yvchandrasekhar88@gmail.com>
2024-06-05 22:34:54 +0530

    Delete BookStoreAPI directory

--- BookStoreAPI/Data/Books.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookStoreAPI.Data
-{
-    public class Books
-    {
-        public int id { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-    }
-}
